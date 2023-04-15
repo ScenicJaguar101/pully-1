15200aa8a174460cd59a83922ce8b66157c3000d
Jackson <21977467+ScenicJaguar101@users.noreply.github.com>
2023-04-14 21:53:49 -0400

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,9 @@
 // TODO: implement the tests
 console.log("Tests are not implemented yet...");
+
+
+const index = require("../index")
+console.log("Tests are not implemented yet...");
+index.ls(".");
+index.cat("./index.js")
+setTimeout(() => { console.log("World!"); }, 5000);
